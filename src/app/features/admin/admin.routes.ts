2a8368390c93bdbff4942ebc52bf88f0187300a2
DerPatrice <patricelechner88@gmail.com>
2026-03-17 22:44:34 +0100

    added admin.routes.ts

---
+++ src/app/features/admin/admin.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+import {AdminHome} from './admin-home/admin-home';
+import {RoomManagement} from './room-management/room-management';
+import {EventManagement} from './event-management/event-management';
+import {UserManagement} from './user-management/user-management';
+
+export const adminRoutes: Routes = [
+  {
+    path: '',
+    component: AdminHome
+  },
+  {
+    path: 'event-management',
+    component: EventManagement
+  },
+  {
+    path: 'room-management',
+    component: RoomManagement
+  },
+  {
+    path: 'user-management',
+    component: UserManagement
+  },
+  {
+    path: 'exam-management',
+    component: EventManagement
+  },
+];
