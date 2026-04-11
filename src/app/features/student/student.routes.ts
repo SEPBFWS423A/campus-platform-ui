eb80b3a623358b4f4d0b9ab3bc66d255b794b9f0
MauritsWolf <mauris.mvp@gmail.com>
2026-04-11 05:57:29 +0200

    Added Studentupload, Visual Status

--- src/app/features/student/student.routes.ts
+++ src/app/features/student/student.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import {StudentHome} from './student-home/student-home';
-import {Grades} from './grades/grades';
-import {Submissions} from './submissions/submissions';
-import {Timetable} from './timetable/timetable';
+import { StudentHome } from './student-home/student-home';
+import { Grades } from './grades/grades';
+import { Submissions } from './submissions/submissions';
+import { Timetable } from './timetable/timetable';
 
 export const studentRoutes: Routes = [
   {
